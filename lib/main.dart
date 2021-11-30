b507258a6afb67de4f867d5ba3ff4b331b23f1eb
jho3r <jhoer.perez@gmail.com>
2021-11-30 00:51:04 -0500

    Implementando providers al proyecto

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,8 @@ import 'widgets/gradient_back.dart';
 import 'Place/ui/screens/header_appbar.dart';
 import 'package:flutter/services.dart';
 import 'platzi_trips.dart';
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
+import 'package:platzi_trips_app/User/bloc/bloc_user.dart';
 
 void main() {
   runApp(MyApp());
@@ -26,22 +28,12 @@ class MyApp extends StatelessWidget {
       )
     );
 
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: PlatziTrips()//const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    return BlocProvider(
+        child: MaterialApp(
+          title: 'Flutter Demo',
+          home: PlatziTripsCupertino(),
+        ),
+        bloc: UserBloc());
   }
 }
 
